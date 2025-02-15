5bd5febde1a752f92bd2241306bed2780f6dec70
Zoey <Zoeykwok8@gmail.com>
2025-02-15 19:57:50 +0000

    didiidididnt finishshhshshshs  yup

---
+++ script.js
@@ -0,0 +1,24 @@
+function intitialize() {
+    console.log("********************");
+    console.log("PROGRAM INITILAIZED");
+    console.log("********************
+}
+
+function toQuarters(dollars) {
+    let quarters = dollarts * 4;
+    console.log("$" + dollar + " is worth " + quarters + " quarters");
+}
+
+intitialize();
+
+toQuarters(5);
+
+toQuarter(10);
+
+intitialize();
+
+toQuarters(2);
+
+intitialize();
+
+toQuarters(0); 
